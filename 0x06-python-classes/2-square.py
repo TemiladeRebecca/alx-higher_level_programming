eb634907700b21d5ae4fc203547f93386df2a3ed
TemiladeRebecca <temilade.rebecca20@gmail.com>
2023-06-27 13:19:30 -0700

    Finding square with size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defines a class Square"""
+"""Square class definition"""
 
 
 class Square:
@@ -8,19 +8,12 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-    def __init__(self, size=0):
-        """initializes the square
+    def __init__(self, size):
+        """Initializes a square
 
         Args:
             size (int): size of a side of the square
 
-        Returns:
-            None
+        Returns: None
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        self.__size = size
