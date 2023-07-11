5c35b8c7ca2a19f23fb721ff0c4bbc3944db4d86
TemiladeRebecca <temilade.rebecca20@gmail.com>
2023-07-11 15:09:37 -0700

    Function that returns the dictionary description with simple data structure for JSON serialization of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""module documentation"""
+"""
+Contains the "class_to_json" function
+"""
 
 
 def class_to_json(obj):
-    """
-    function that returns a dictionary description
-    for json serialization
-    """
-    return obj.__dict__
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return obj.__dict___
