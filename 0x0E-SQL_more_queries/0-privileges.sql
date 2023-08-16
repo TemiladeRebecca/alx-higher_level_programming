cf775c78359a5d7a879453c63f0f0c5908c1ea93
TemiladeRebecca <temilade.rebecca20@gmail.com>
2023-08-16 14:55:05 -0700

    Listing all privileges of the MySQL users user_0d_1 and user_0d_2 on your server

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
-
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- lists all privileges of MySQL users in MySQL servers
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'USER_0d_2'@'localhost';
