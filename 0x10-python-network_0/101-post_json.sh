8e75b39c5406b00c438ffda12990b0cd240c5b65
TemiladeRebecca <Temilade.Rebecca20@gmail.com>
2023-09-28 14:25:50 -0700

    Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a given URL with a given JSON file.
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# -H = Specify header content type to be application/json.
-# -d = Specify HTTP POST data to post.
-# cat "$2" = Read the contents of the JSON file.
-
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# posts json file
+curl -s -X POST -d @"$2" -H "Content-Type: application/json" "$1"
