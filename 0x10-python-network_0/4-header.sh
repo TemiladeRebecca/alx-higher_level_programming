c73a32da226072395ee1b2f2917aaaa620b68010
TemiladeRebecca <Temilade.Rebecca20@gmail.com>
2023-09-28 14:07:46 -0700

    Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# -H = Pass custom header(s) to server. Can be used for Authentication.
-# X-School-User-Id = The header variable.
-curl -sH "X-School-User-Id: 98" "$1"
+# sends GET req with header var
+curl -s -H "X-School-User-Id:98" "$1"
