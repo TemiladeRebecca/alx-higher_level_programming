676fa454a7b92bec8b50b589a176008410236a6e
TemiladeRebecca <Temilade.Rebecca20@gmail.com>
2023-09-28 13:57:22 -0700

    Script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# -L = Follow redirects in URL
-curl -sL "$1"
+# displays body of 200 status code response
+curl -sL $1
